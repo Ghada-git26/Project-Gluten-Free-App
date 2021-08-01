a65fd4294049f30ff62241bfe234cd8c45034df1
Ghada <ghadamannai9@gmail.com>
2021-08-01 23:39:07 +0200

    initial commit + recipeModel

---
+++ models/recipe.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    prep: String,
+    cook: String,
+    difficultie: String,
+    nutrition: {
+        kcal: String,
+        carbs: String,
+        fibre: String,
+        protein: String,
+        sugars: String,
+        salts: String,
+    },
+    ingridients: [String],
+    image: String,
+    rate: [{ type: Schema.Types.ObjectId, ref: "Rate" }],
+
+});
+
+const recipeModel = mongoose.model("recipe", recipeSchema);
+
+module.exports = recipeModel;
